cf01703216c4d708f4ff948a32b127159b15e24f
Matt Martin <matt.martin@sonicdrivein.com>
2018-06-02 16:49:38 -0500

    footer tweaks.

--- src/containers/footer_player.js
+++ src/containers/footer_player.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from "react-redux";
 import '../css/Footer.css'
+import logo from '../images/FBC_Logo.png'
 
 class FooterPlayer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             duration: null,
-            currentTime: 0
+            currentTime: 0,
+            width: window.innerWidth,
         }
     }
 
     handlePlay() {
-
 
 
         this.audio.play();
@@ -27,14 +28,14 @@ class FooterPlayer extends Component {
     }
 
 
-    seekToTime(e){
+    seekToTime(e) {
         this.audio.currentTime = e.target.value;
 
     }
 
-    componentDidUpdate(){
+    componentDidUpdate() {
 
-        if(this.audio){
+        if (this.audio) {
             this.audio.onplay = () => {
                 this.currentTimeInterval = setInterval(() => {
                     this.setState({duration: this.audio.duration, currentTime: this.audio.currentTime});
@@ -43,93 +44,153 @@ class FooterPlayer extends Component {
 
         }
     }
+
     componentWillUnmount() {
         clearInterval(this.currentTimeInterval);
         //this.audio = null;
     }
-    componentDidMount() {
-            /*this.currentTimeInterval = null;
 
-            // Get duration of the song and set it as max slider value
-            this.audio.onloadedmetadata = function () {
-                this.setState({duration: this.audio.duration});
-            }.bind(this);
-
-            // Sync slider position with song current time
-            this.audio.onplay = () => {
-                this.currentTimeInterval = setInterval(() => {
-                    this.slider.value = this.audio.currentTime;
-                }, 500);
-            };
-
-            this.audio.onpause = () => {
-                clearInterval(this.currentTimeInterval);
-            };
-
-            // Seek functionality
-            this.slider.onchange = (e) => {
-
-                clearInterval(this.currentTimeInterval);
-                this.audio.currentTime = e.target.value;
-            };*/
+    renderMobileFooter(currentDisplay, durationDisplay){
+        const src = this.props.sermon['mp3URI'];
+        const imageURL = this.props.sermon.imageURI != null ? this.props.sermon.imageURI : "http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/128/Headphones-Apollo-icon.png";
 
 
-    }
+        var title = (this.props.sermon.title.length > 45) ?
+            this.props.sermon.title.substring(0, 45) + "..." :
+            this.props.sermon.title;
 
-
-
-    render(){
-
-        function pad(str, size=2) {
-            while (str.length < (size)) {str = "0" + str;}
-            return str;
-          }
-          
-
-        if(this.props.sermon) {
-            const src = this.props.sermon['mp3URI'];
-
-            var current_min = Math.floor(this.state.currentTime / 60);
-            var current_seconds = Math.round((this.state.currentTime - current_min * 60));
-            var currentDisplay = `${pad(current_min.toString())}:${pad(current_seconds.toString())}`;
-
-            var duration_min = Math.floor(this.state.duration / 60);
-            var duration_seconds = Math.round((this.state.duration - duration_min * 60));
-            var durationDisplay = `${pad(duration_min.toString())}:${pad(duration_seconds.toString())}`;
-
-            return (
-                <div id='stickyFooter'>
-                <div className='playerTitle'>{this.props.sermon.title}</div>
-                    <div className={"playerWrapper"}>
-                        <div className={"controls"}>
-                            <i className="material-icons" onClick={ this.handlePlay.bind(this) }>play_circle_outline</i>
-                            <i className="material-icons" onClick={ this.handleStop.bind(this) }>pause</i>
+        return(
+            <div id='stickyFooterMobile'>
+                <div>
+                    <div className={"playerWrapperMobile"}>
+                        <div className={"playerImg"}>
+                            <img className='img-fluid' src={imageURL}/>
                         </div>
 
+                        <div>
+
+                            <div className={"playerSermonDetails"}>
+                                <div className={'playerSeries'}>{this.props.sermon.series}</div>
+                                <div className='playerTitle'>{title}</div>
+                                <div className={'playerSpeaker'}>{this.props.sermon.speaker}</div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className={"playerMobileControlsWrapper"}>
+                        <div className={"controlsMobile"}>
+                            <i className="material-icons md-22" onClick={this.handlePlay.bind(this)}>play_circle_outline</i>
+                            <i className="material-icons md-22" onClick={this.handleStop.bind(this)}>pause</i>
+                        </div>
                         <div className={"audio"}>
-                            <audio ref={(audio) => { this.audio = audio }} src={src} autoPlay={true}/>
+                            <audio ref={(audio) => {
+                                this.audio = audio
+                            }} src={src} autoPlay={true}/>
                         </div>
-                        
                         <div className={"currentTime"}>{currentDisplay}</div>
                         <div className={"progressBar"}>
-                            
+
                             <p><input ref={(slider) => {
-                                this.slider = slider }}
+                                this.slider = slider
+                            }}
                                       type="range"
                                       name="points"
                                       value={this.state.currentTime}
                                       onChange={this.seekToTime.bind(this)}
-                                      min="0" max={this.state.duration} /> </p>
+                                      min="0" max={this.state.duration}/></p>
                         </div>
                         <div className={"duration"}>{durationDisplay}</div>
                     </div>
                 </div>
 
 
-            );
+            </div>
+        );
+    }
+
+    renderDesktopFooter(currentDisplay, durationDisplay){
+
+        const src = this.props.sermon['mp3URI'];
+        const imageURL = this.props.sermon.imageURI != null ? this.props.sermon.imageURI : "http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/128/Headphones-Apollo-icon.png";
+
+        return (
+            <div id='stickyFooter'>
+
+                <div className={"playerWrapper"}>
+                    <div className={"playerImg"}>
+                        <img className='img-fluid' src={imageURL}/>
+                    </div>
+                    <div className={"playerSermonDetails"}>
+                        <div className={'playerSeries'}>{this.props.sermon.series} ({this.props.sermon.date})</div>
+                        <div className='playerTitle'>{this.props.sermon.title}</div>
+                        <div className={'playerSpeaker'}>{this.props.sermon.speaker}</div>
+
+                    </div>
+                    <div className={"controls"}>
+                        <i className="material-icons" onClick={this.handlePlay.bind(this)}>play_circle_outline</i>
+                        <i className="material-icons" onClick={this.handleStop.bind(this)}>pause</i>
+                    </div>
+
+                    <div className={"audio"}>
+                        <audio ref={(audio) => {
+                            this.audio = audio
+                        }} src={src} autoPlay={true}/>
+                    </div>
+
+                    <div className={"currentTime"}>{currentDisplay}</div>
+                    <div className={"progressBar"}>
+
+                        <p><input ref={(slider) => {
+                            this.slider = slider
+                        }}
+                                  type="range"
+                                  name="points"
+                                  value={this.state.currentTime}
+                                  onChange={this.seekToTime.bind(this)}
+                                  min="0" max={this.state.duration}/></p>
+                    </div>
+                    <div className={"duration"}>{durationDisplay}</div>
+                </div>
+            </div>
+
+
+        );
+    }
+
+
+    render() {
+
+        function pad(str, size = 2) {
+            while (str.length < (size)) {
+                str = "0" + str;
+            }
+            return str;
         }
-        else{
-            return(<div></div>);
+
+        const { width } = this.state;
+        const isMobile = width <= 768;
+
+        var current_min = Math.floor(this.state.currentTime / 60);
+        var current_seconds = Math.round((this.state.currentTime - current_min * 60));
+        var currentDisplay = `${pad(current_min.toString())}:${pad(current_seconds.toString())}`;
+
+        var duration_min = Math.floor(this.state.duration / 60);
+        var duration_seconds = Math.round((this.state.duration - duration_min * 60));
+        var durationDisplay = `${pad(duration_min.toString())}:${pad(duration_seconds.toString())}`;
+
+
+        if (this.props.sermon) {
+            if(isMobile){
+                return this.renderMobileFooter(currentDisplay, durationDisplay);
+            }
+            else{
+                return this.renderDesktopFooter(currentDisplay, durationDisplay);
+            }
+        }
+        else {
+            return (<div></div>);
         }
     }
 }
@@ -142,10 +203,9 @@ function mapStateToProps(state) {
 }
 
 
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
 
-    return {
-    }
+    return {}
 }
 
 export default connect(
